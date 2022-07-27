a48286cbae44ba602a5b5269350df4d665cba6c7
ReddNev <creedlll@mail.ru>
2022-07-27 13:52:56 +0300

    update

--- itfinder/projects/urls.py
+++ itfinder/projects/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.projects, name='projects'),
     path('project-object/<str:pk>/', views.project, name='project'),
-    path('create/', views.createProject, name='create-project'),
-    path('update-project/<str:pk>', views.updateProject, name="update-project"),
+    path('create/', views.create_project, name='create-project'),
+    path('update-project/<str:pk>', views.update_project, name="update-project"),
     path('tag/<slug:tag_slug>', views.projects_by_tag, name="tag"),
 ]
