111e422b5799c70ca9882df8998be6a7f1e7e764
ReddNev <creedlll@mail.ru>
2022-08-04 12:35:03 +0300

    update

--- itfinder/projects/views.py
+++ itfinder/projects/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Project, Tag
-from .forms import ProjectForm
+from .forms import ProjectForm, ReviewForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
@@ -14,7 +14,18 @@ def projects(request):
 def project(request, project_slug):
     project = Project.objects.get(slug=project_slug)
     tags = project.tags.all()
-    return render(request, 'projects/single-project.html', {'project': project})
+    form = ReviewForm()
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        review = form.save(commit=False)
+        review.project = project
+        review.owner = request.user.profile
+        review.save()
+        project.getVoteCount
+        messages.success(request, 'Ваш отзыв был добавлен!')
+        return redirect('project', project_slug=project.slug)
+    return render(request, 'projects/single-project.html', {'project': project, 'form': form})
 
 
 @login_required(login_url="login")
