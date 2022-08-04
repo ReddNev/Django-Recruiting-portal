ef1980c0775e3fa37fa839a2851e2a339bbcb805
ReddNev <creedlll@mail.ru>
2022-08-04 09:32:33 +0300

    [FIX]

--- itfinder/projects/urls.py
+++ itfinder/projects/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.projects, name="projects"),
     path('project/<slug:project_slug>/', views.project, name="project"),
     path('tag/<slug:tag_slug>', views.projects_by_tag, name="tag"),
-    path('create-project/', views.createProject, name="create-project"),
-    path('update-project/<str:pk>', views.updateProject, name="update-project"),
-    path('delete-project/<str:pk>', views.deleteProject, name="delete-project"),
+    path('create-project/', views.create_project, name="create-project"),
+    path('update-project/<str:pk>', views.update_project, name="update-project"),
+    path('delete-project/<str:pk>', views.delete_project, name="delete-project"),
 ]
--- itfinder/projects/views.py
+++ itfinder/projects/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import ProjectForm
 from .models import Project, Tag
+from .forms import ProjectForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 def projects(request):
@@ -9,42 +11,65 @@ def projects(request):
     return render(request, 'projects/projects.html', context)
 
 
-def project(request, pk):
-    projectObj = Project.objects.get(id=pk)
-    tags = projectObj.tags.all
-    return render(request, 'projects/single-project.html', {'project': projectObj})
+def project(request, project_slug):
+    project = Project.objects.get(slug=project_slug)
+    tags = project.tags.all()
+    return render(request, 'projects/single-project.html', {'project': project})
 
 
+@login_required(login_url="login")
 def create_project(request):
+    profile = request.user.profile
     form = ProjectForm()
+
     if request.method == 'POST':
+        newtags = request.POST.get('newtags').replace(',', " ").split()
         form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('projects')
+            project = form.save(commit=False)
+            project.owner = profile
+            project.save()
+
+            for tag in newtags:
+                tag, created = Tag.objects.get_or_create(name=tag)
+                project.tags.add(tag)
+            return redirect('account')
+
     context = {'form': form}
-    return render(request, 'projects/project_form.html', context)
+    return render(request, "projects/project_form.html", context)
 
 
+@login_required(login_url="login")
 def update_project(request, pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
     form = ProjectForm(instance=project)
+
     if request.method == 'POST':
+        newtags = request.POST.get('newtags').replace(',', " ").split()
+
         form = ProjectForm(request.POST, request.FILES, instance=project)
         if form.is_valid():
-            form.save()
-            return redirect('projects')
-    context = {'form': form}
-    return render(request, 'projects/project_form.html', context)
+            project = form.save()
+            for tag in newtags:
+                tag, created = Tag.objects.get_or_create(name=tag)
+                project.tags.add(tag)
+
+            return redirect('account')
+
+    context = {'form': form, 'project': project}
+    return render(request, "projects/project_form.html", context)
 
 
+@login_required(login_url="login")
 def delete_project(request, pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
     if request.method == 'POST':
         project.delete()
         return redirect('projects')
     context = {'object': project}
-    return render(request, 'projects/delete.html', context)
+    return render(request, 'delete_template.html', context)
 
 
 def projects_by_tag(request, tag_slug):
